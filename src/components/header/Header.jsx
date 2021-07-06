c9c44a27beaa80600cfeaed73a9b179c7bb820e8
RomanMakatsaryia <hazget91@gmail.com>
2021-07-06 17:33:55 +0300

    First commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">Fanfic</span>
+      </div>
+      <img
+        className="headerImg"
+                src="https://images4.alphacoders.com/674/thumb-1920-674230.jpg"
+                alt=""
+            />
+        </div>
+    );
+}
+
